8b2b0b63df1ae7e2c55bf89cd16fb255716031f0
Nocturnal <contact@nocturnal.solutions>
2018-02-22 15:54:33 -0700

    Okay, we need to do less here.

--- Sources/CredentialsLocal/CredentialsLocal.swift
+++ Sources/CredentialsLocal/CredentialsLocal.swift
@@ -8,32 +8,22 @@ public class CredentialsLocal: CredentialsPluginProtocol {
 
     public var usersCache: NSCache<NSString, BaseCacheElement>?
 
-    public var redirecting: Bool
-
-    /// Path user should be redirected to if not logged in; nil if no
-    /// redirection should happen.
-    public let loginPagePath: String?
+    // Our plugin does redirect, but not for the sake of logging in, even though
+    // it does. I guess what I'm saying is that this parameter confuses me.
+    public var redirecting: Bool = false
 
     public func authenticate(request: RouterRequest, response: RouterResponse, options: [String : Any], onSuccess: @escaping (UserProfile) -> Void, onFailure: @escaping (HTTPStatusCode?, [String : String]?) -> Void, onPass: @escaping (HTTPStatusCode?, [String : String]?) -> Void, inProgress: @escaping () -> Void) {
         // That this method got called means that a user session was not loaded.
-        if let redirectTo = loginPagePath {
-            try? response.redirect(redirectTo)
+        if options.index(forKey: "failureRedirect") != nil {
+            // Trigger the redirect.
             inProgress()
         }
         else {
+            // Show access denied page.
             onFailure(nil, nil)
         }
     }
 
-    /// Initialize with an optional login page path.
-    ///
-    /// - Parameter loginPagePath: If nil, a non-authenticated user will be
-    ///                            shown an error page; otherwise they will be
-    ///                            redirected to this path, with a GET parameter
-    ///                            of "redirectTo" corresponding to the path
-    ///                            they were trying to access.
-    public init(loginPagePath: String? = nil) {
-        self.loginPagePath = loginPagePath
-        redirecting = self.loginPagePath != nil
-    }
+    public init() {}
 }
+
